4f1e40ba72e2b696c700022dd4439fb7dc3147b5
Max Akimov <maxymus2828@gmail.com>
2026-02-16 23:34:38 +0200

    corrected type

--- src/types/note.ts
+++ src/types/note.ts
@@ -7,4 +7,4 @@ export interface Note {
   tag: NoteTag;
 }
 
-export type NoteTag = "Work" | "Meeting" | "Personal" | "Shopping" | "TODO";
+export type NoteTag = "Work" | "Meeting" | "Personal" | "Shopping" | "Todo";
